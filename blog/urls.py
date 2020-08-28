7d3a193385230c63c3458c153fc0132082a4dcf2
Alfred Grealis <grealisalfred@gmail.com>
2020-08-28 12:41:46 +0100

    Icon]

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -7,4 +9,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/new/', views.post_new, name='post_new'),
     path('cool_work', views.cool_work, name='cool_work'),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/favicon.ico')),
 ]
